7d8a1b29cd87baaa7b3a26536a0d37f44d821f74
Hussaini Usman <hussainmaina27@gmail.com>
2022-12-22 06:32:20 +0100

    add blurDataURL and placeholder to hero section technologies images

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,8 @@ export default function Home() {
           width="50"
           height="50"
           className="w-full h-full object-contain"
+          blurDataURL={source}
+          placeholder="blur"
         />
       </div>
     );
