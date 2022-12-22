228afd46009b5409be1c8ecfa78b4bff3f880332
Hussaini Usman <hussainmaina27@gmail.com>
2022-12-22 07:13:03 +0100

    add link to github button

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -101,13 +101,13 @@ const Layout = ({ children }) => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
+                <a
+                  href="https://github.com/Housain-maina"
+                  target="_blank"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
-                  <span className="sr-only">Open Github</span>
                   <Github color="white" size="24" />
-                </button>
+                </a>
               </div>
             </div>
           </div>
