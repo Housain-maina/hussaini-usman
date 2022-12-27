7fb3b08273818bc562d262128540114cd02e82ba
Hussaini Usman <hussainmaina27@gmail.com>
2022-12-27 14:42:29 +0100

    setup route change progress bar indicator

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,16 @@ import { DefaultSeo } from "next-seo";
 import "../styles/globals.css";
 import Layout from "@/components/Layout";
 import SEO from "../next-seo.config";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <NextNProgress
+        color="#0096C7"
+        height={3}
+        options={{ showSpinner: false }}
+      />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
