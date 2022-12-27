7356dd52c4fb47ee7dc2a811ba6be3b905588917
Hussaini Usman <hussainmaina27@gmail.com>
2022-12-27 08:24:56 +0100

    add status bar theme color

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#1F2937" />
+        <meta name="msapplication-navbutton-color" content="#1F2937" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#1F2937" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
