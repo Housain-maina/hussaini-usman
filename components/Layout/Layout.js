a37100b1b07b092422a767e3629d92f0a3a750e7
Hussaini Usman <hussainmaina27@gmail.com>
2022-12-31 06:58:02 +0100

    remove chevron icon hover style

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               <button
                 type="button"
-                className="inline-flex items-center justify-center rounded-md text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                className="inline-flex items-center justify-center rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
                 onClick={() => {
