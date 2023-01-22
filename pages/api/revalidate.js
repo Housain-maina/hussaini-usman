6d6fbadf75d902c287ff4242d265671b80383d6a
Hussaini Usman <hussainmaina27@gmail.com>
2023-01-22 07:50:15 +0100

    modity revalidate api route to accept path from request body

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,4 +1,11 @@
 export default async function handler(req, res) {
+  // check for the POST request
+  if (req.method !== "POST") {
+    return res
+      .status(400)
+      .json({ error: "Invalid HTTP method. Only POST requests are allowed." });
+  }
+
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
@@ -7,8 +14,18 @@ export default async function handler(req, res) {
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate(req.query.path);
-    return res.json({ revalidated: true });
+    const body = req.body;
+    if (!body) {
+      res.status(400).send("Bad request (no body)");
+      return;
+    }
+
+    // get the slug to revalidate from body
+    const path = body.path;
+    if (path) {
+      await res.revalidate(path);
+      return res.json({ revalidated: true });
+    }
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
