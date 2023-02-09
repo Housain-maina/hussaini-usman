4eaf488d29dd4b5840db733f2d867e377868d234
Hussaini Usman <hussainmaina27@gmail.com>
2023-02-09 07:01:59 +0100

    add google analytics tags

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,34 @@ import "../styles/globals.css";
 import Layout from "@/components/Layout";
 import SEO from "../next-seo.config";
 import NextNProgress from "nextjs-progressbar";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <NextNProgress
-        color="#0096C7"
-        height={3}
-        options={{ showSpinner: false }}
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
       />
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </Layout>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
+        `}
+      </Script>
+      <Layout>
+        <NextNProgress
+          color="#0096C7"
+          height={3}
+          options={{ showSpinner: false }}
+        />
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
